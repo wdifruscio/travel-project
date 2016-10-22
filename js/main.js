799d5489ffb5ef41e23fec51dbb82f4f4f6ea5a5
Will DiFruscio <wdifruscio@gmail.com>
2016-10-21 23:20:04 -0400

    changed some stuff

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@ myApp.endCities = [];
 //vars for some functions
 var $results = $('#results');
 var $showActivites = $('.section___activity');
-var $loader = $('.loader');
+var $loaderOne= $('#loaderOne');
+var $loderTwo= $('#loaderTwo');
 
-//scroll down and fadeIn fnct
+//scroll down and fadeIn fnct for map
 function mapClick(){
     myApp.findCityList();
     $showActivites.fadeIn();
@@ -17,18 +18,20 @@ function mapClick(){
             window.location.href='#section___activity';
         },50);
 };
-
+function submitClick() {
+    $results.fadeIn();
+    $('.section___results').show();
+};
 
 //label onclick funct
 $('label').on('click',function(){
-    $(this).toggleClass('blue');
+    $(this).toggleClass('teal');
 });
 
 //form submit
 $('form').on('submit',function(e){
     e.preventDefault();
-    $results.css('opacity', '1');
-    $results.css('height', 'auto');
+    submitClick();
     myApp.findCityWithactivities(myApp.activitiesCities);
 });
 
@@ -79,7 +82,7 @@ myApp.findCityList = function(){
         url: "https://nomadlist.com/api/v2/list/cities",
         dataType: "json"
     }).then(function(res){
-        $loader.hide();
+        $loaderOne.hide();
         $('form').fadeIn();
         $('form').css('display','flex');
         $("#form___title").fadeIn();   
@@ -103,12 +106,12 @@ myApp.findCityWithactivities = function(listOfSelectedCities){
     var $hiking = $("#hiking").is(":checked");
     var $history = $("#history").is(":checked");
     var $outdoors = $("#outdoors").is(":checked");
-    var $streetfood = $("#streetfood").is(":checked");
+    var $surfing = $("#surfing").is(":checked");
     var $cycling = $("#cycling").is(":checked");
     var $beach = $("#beach").is(":checked");
     var $temples = $("#temples").is(":checked");
     var $spa = $("#spa").is(":checked");
-    var $backpacker = $("#backpacker").is(":checked");
+    var $golf = $("#golf").is(":checked");
     
   var activityFilteredCities = myApp.activitiesCities.filter(function(citiesWithactivities){
       for(var i = 0; i < citiesWithactivities.length; i++){
@@ -119,7 +122,7 @@ myApp.findCityWithactivities = function(listOfSelectedCities){
           if (citiesWithactivities[i].tags.includes('outdoors') && $outdoors) { 
              myApp.endCities.push(citiesWithactivities[i]);
         }
-          if (citiesWithactivities[i].tags.includes('streetfood') && $streetfood) { 
+          if (citiesWithactivities[i].tags.includes('surfing') && $surfing) { 
              myApp.endCities.push(citiesWithactivities[i]);
         }
           if (citiesWithactivities[i].tags.includes('cycling') && $cycling) { 
@@ -134,10 +137,15 @@ myApp.findCityWithactivities = function(listOfSelectedCities){
           if (citiesWithactivities[i].tags.includes('spa') && $spa ) { 
              myApp.endCities.push(citiesWithactivities[i]);
         }            
-          if (citiesWithactivities[i].tags.includes('backpacker') && $backpacker) { 
+          if (citiesWithactivities[i].tags.includes('hiking') && $hiking) { 
              myApp.endCities.push(citiesWithactivities[i]);
         }
-          if(!$backpacker && !$history &&!$outdoors &&!$hiking && !$streetfood && !$cycling && !$beach && !$spa && !$temples && !$backpacker){
+
+          if (citiesWithactivities[i].tags.includes('golf') && $golf) { 
+             myApp.endCities.push(citiesWithactivities[i]);
+        }
+
+          if(!$history &&!$outdoors &&!$hiking && !$surfing && !$cycling && !$beach && !$spa && !$temples && !$golf){
               myApp.endCities.push(citiesWithactivities[i]);
           }                               
     }
@@ -156,27 +164,33 @@ myApp.displayCities = function(finalArray){
         var safetyScoreNumber = individualCity.scores.safety * 250;
     
         var $results = $("#results");
-        var $cityContainer = $('<figure>');
+        var $cityContainer = $('<figure class="figure___city">');
 
-        var $cityHeader = $('<div>').css(
+        var $cityHeader = $('<div class="cityHeader">').css(
             "background-image", "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(https://nomadlist.com" +individualCity.media.image['250']+")"
         );
 
-        var $cityName = $("<h3>").text(individualCity.info.city.name);
-        var $countryName = $("<h4>").text(individualCity.info.country.name);
-        var $leisureLabel = $('<p>').text("Leisure Rating");
+        var $cityName = $("<h3 class='city___name'>").text(individualCity.info.city.name);
+        var $countryName = $("<h4 class='country___name'>").text(individualCity.info.country.name);
+        var $leisureLabel = $('<p class="paragraph___top">').text("Leisure Rating");
         var $leisureScore = $('<div class="progress ' + ((leisureScoreNumber < 75) ? 'red ' : 'green ')+((leisureScoreNumber < 175 && leisureScoreNumber > 75) ? 'yellow ' : 'green ') + '">').width(leisureScoreNumber);
         var $nightlifeLabel = $('<p>').text("Nightlife Rating");
         var $nightlifeScore = $('<div class="progress ' + ((nightlifeScoreNumber < 75) ? 'red ' : 'green ')+((nightlifeScoreNumber < 175 && nightlifeScoreNumber > 75) ? 'yellow ' : 'green ') + '">').width(nightlifeScoreNumber);
         var $safetyLabel = $('<p>').text("Safety Rating");
         var $safetyScore = $('<div class="progress ' + ((safetyScoreNumber < 75) ? 'red ' : 'green ')+((safetyScoreNumber < 175 && safetyScoreNumber > 75) ? 'yellow ' : 'green ') + '">').width(safetyScoreNumber);
         var $airbnbScore = $('<p>').text("Airbnb average one night stay: $" + individualCity.cost.airbnb_median.USD);
-        var $imageAnchor = $('<a href="http://www.facebook.com">').text("click here for some photos");
+        var $imageAnchor = $('<a href="" class="view___pix swipebox">').text("Photos");
+        var $airBnbAnchor = $('<a href="https://www.airbnb.ca/s/' + individualCity.info.city.name + '" target="_blank">').text("Air Bnb");
 
 
         $cityHeader.append($cityName,$countryName);
-        $cityContainer.append($cityHeader,$leisureLabel,$leisureScore,$nightlifeLabel,$nightlifeScore,$safetyLabel,$safetyScore,$airbnbScore,$imageAnchor);
+        $cityContainer.append($cityHeader,$leisureLabel,$leisureScore,$nightlifeLabel,$nightlifeScore,$safetyLabel,$safetyScore,$airbnbScore,$imageAnchor,$airBnbAnchor);
+        $results.flickity({
+            "pageDots": false,
+            "wrapAround":true
+        });
         $results.flickity('append',$cityContainer);
+        window.location.href='#results';
     });
 }
 
@@ -187,6 +201,8 @@ myApp.displayCities = function(finalArray){
 myApp.init = function(){
     $('header').vide('videos/bgvid');
     worldMap();
+    console.log('init');
+
 }; //init end
 
 $(document).ready(function(){ //doc rdy
@@ -208,3 +224,40 @@ $(function() {
     }
   });
 });
+
+
+$('.section___results').on('click',".view___pix", function(e){
+    e.preventDefault(); 
+    var cityQuery = $(this).siblings('.cityHeader').children('.city___name').text();
+    var countryQuery = $(this).siblings('.cityHeader').children('.country___name').text();
+    console.log(countryQuery);
+    myApp.getPhotos(cityQuery);    
+});
+
+myApp.getPhotos = function(myQuery) {
+    $.ajax({
+        url: 'https://pixabay.com/api/',
+        dataType : "JSON",
+        data: {
+        key: '3551643-fa4c0e3a2956d8c2df20a82df',
+            category: 'travel',
+            image_type: 'photo',
+            q: myQuery,
+        }
+    }).then(function(photoRes){
+        $('#loaderTwo').hide();
+        console.log(photoRes);
+        myApp.displayPhotos(photoRes);
+    });
+}
+
+myApp.displayPhotos = function(countryPhotos) {
+    var thesePics = countryPhotos.hits;
+    if (thesePics.length > 0){
+        var gallery = thesePics.map(item => ({href: item.webformatURL}));
+        $.swipebox(gallery);
+    }
+    else{
+        alert("No pictures found, sorry!");
+    }
+};  
